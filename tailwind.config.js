8c95c03330709dcd31656f624d07a53b5a0cac26
Jiseeeh <johncarlo.camara1@gmail.com>
2022-10-25 15:17:23 +0800

    style: extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#393E46",
+        "dark-primary": "#222831",
+        secondary: "#EEEEEE",
+        accent: "#00ADB5",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
