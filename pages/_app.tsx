ef4e812566e78544a01d7a2187ca94b6d5d43ccc
Jiseeeh <johncarlo.camara1@gmail.com>
2023-01-14 01:23:04 +0800

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+
 import { SharedThemeProvider } from "../lib/DarkModeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SharedThemeProvider>
-      <Component {...pageProps} />
-    </SharedThemeProvider>
+    <>
+      <SharedThemeProvider>
+        <Component {...pageProps} />
+      </SharedThemeProvider>
+      <Analytics />
+    </>
   );
 }
 
